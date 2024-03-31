ac293e176b90c4ec68c05ca047723df729ccb50f
supriyamogal <supriyakadam341@gmail.com>
2024-03-31 15:56:46 +0530

    new file created

---
+++ IfElseDemo.java
@@ -0,0 +1,12 @@
+public class IfElseDemo{
+public static void main(String[]args){
+int personAge=15;
+if(personAge>=18){
+System.out.println("You are eligible for vote");
+}
+else{
+System.out.println("You are not eligible for vote");
+
+}
+}
+}
